ae369a5629c4880086b3331fb099b98d16d66c35
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-11-23 09:00:06 -0300

    feat: add app icon

---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Luiz Araujo on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
